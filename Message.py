6d7d7085f0d3c74a123514255a2b71ea913b737a
Laurent-St <laurent-st@hotmail.com>
2017-11-24 11:24:08 +0100

    test generation nbre premier

--- Message.py
+++ Message.py
@@ -5,7 +5,7 @@ class Message:
 
 class KEY_INIT(Message):
     def __init__(self,version,type_,msg_length,key_id,g,p,A):
-        MySuperClass.__init__(self,version,type_)
+        Message.__init__(self,version,type_)
         self.msg_length=msg_length
         self.key_id=key_id
         self.g=g
@@ -14,14 +14,14 @@ class KEY_INIT(Message):
 
 class KEY_REPLY(Message):
     def __init__(self,version,type_,msg_length,key_id,B):
-        MySuperClass.__init__(self,version,type_)
+        Message.__init__(self,version,type_)
         self.msg_length=msg_length
         self.key_id=key_id
         self.B=B
 
 class MESSAGE_RELAY(Message):
     def __init__(self,version,type_,seq_nb,key_id,nexthop,payload,padding):
-        MySuperClass.__init__(self,version,type_)
+        Message.__init__(self,version,type_)
         self.seq_nb=seq_nb
         self.key_id=key_id
         self.nexthop=nexthop
@@ -30,7 +30,7 @@ class MESSAGE_RELAY(Message):
 
 class ERROR(Message):
     def __init__(self,version,type_,msg_length,error_code,padding):
-        MySuperClass.__init__(self,version,type_)
+        Message.__init__(self,version,type_)
         self.msg_length=msg_length
         self.error_code=error_code
         self.padding=padding
