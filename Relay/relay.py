10397fbaf99e18ade0b9e206ad9f05870f439092
bjoukovs <borisjoukov@hotmail.com>
2017-11-26 14:41:28 +0100

    updating relays

--- Relay/relay.py
+++ Relay/relay.py
@@ -1,27 +1,7 @@
-from random import randint
+from relay_config import read_config
 
-class Relay:
-    def __init__(self,ip,port):
-        self._ip=ip
-        self._port=port
-        self._neighbors_costs={}
+data = read_config()
+IP = data[0]    #String
+PORT = data[1]  #Integer
 
-    def connect(self,target_relay,sync,cost=0):
-
-        sync=sync+1
-        if sync <=1:
-            cost=randint(1,9)
-            target_relay.connect(self,sync,cost)
-
-        self._neighbors_costs[target_relay] = cost
-
-
-    @property
-    def neighbors(self):
-        return self._neighbors_costs
-
-    def get_cost(self,neighbor):
-        return self._neighbors_costs[neighbor]
-
-    def get_port(self):
-        return self.port
+print("Relay initialized:",IP,PORT)
--- Relay/relay_config.py
+++ Relay/relay_config.py
@@ -1,1 +1,18 @@
 #This file will contain the code to read the relay config host.ini file
+
+config_url = 'config/host.ini'
+
+def read_config():
+    host_file = open(config_url,'r')
+    host_config = host_file.read().splitlines()
+    host_file.close()
+
+    for line in host_config:
+        if len(line)>0:
+            if line[0]!="#" and line[0]!="[" and line[0]!=" ":
+                elems = line.split(" ")
+
+                IP = elems[0]
+                PORT = int(elems[1])
+    
+    return [IP, PORT]
