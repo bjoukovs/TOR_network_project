62ceb628f798e5a896bd924304e136ccfa78314f
BenjaminWauthion <benjaminwauthion@gmail.com>
2017-11-30 18:42:10 +0100

    clean and comment dijkstra

--- shared/Path_finding_V3.py
+++ shared/Path_finding_V3.py
@@ -2,93 +2,96 @@ from math import inf
 
 def Dijkstra(departure, arrival, network):
 
-    Load_list = {} #liste ou chaque elem à 3 composants (matrice (n+2)*3) premier elem:
-               #le n° du noeud; 2eme elm: le poids du noeud; 3eme elem: v/f pour savor si oui ou non on est passé par ce noeud
-    Past_list = {} #liste ou chaque elem à 2 composants (matrice (n+2)*2) premier elem: le n° du noeud
-                #2eme noeud donne le noeud antérieur: d'ou viens chaque noeud
+    #We are creating 2 dictionnary
+    Load_dic = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
+                    #there is a tuple. 1st element: the load of the node: the total
+                    #cost in order to get to this node. 2nd element: True/False in
+                    #in order to know if the node has already been explored
+    Past_dic = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
+                    #it is assign the previous node, from where come from the message
+                    #before arriving at this node
 
-    nodes = [] #Liste de tous les relais
-    for key, relais in network.items():
+    nodes = [] #List with all the relays
+    for key, relais in network.items(): #allows to get access to all the nodes of the system
         for relai in relais:
             nodes.append(relai)
 
-    Final_path = [arrival] #liste contenant le trajet final de moindre cout
-
-    #je considère le point de départ (Alice) comme un relay à la position 0 et l'arrivée
-    #(Bob) comme un relay à la position n+1 donc la liste de relay est de longeur n + 2 avec
-    #node[0] = depart et node[n+1] = arrivée
+    Final_path = [arrival] #List with the least cost path from alice(departure) to Bob (arrival)
+                            #we already know that this list contain arrival
 
     #initization
-    #Pour le point de départ
-    Load_list[departure] = [0, True] #premier relay
-    Past_list[departure] = None #premier relay
+    #For the departure
+    Load_dic[departure] = [0, True] #The cost is null because we begin from this node and we come
+                                    #from this node: (True)
+    Past_dic[departure] = None #This is the departure, it has no past nodes
 
-    #Pour tout les relays intermédiaires
+    #For all intermediate relays
     for i, node in enumerate(nodes):
         if not(node is arrival) and not(node is departure):
-            Load_list[node] = [inf, False]
-            Past_list[node] = None
+            Load_dic[node] = [inf, False] #The cost for these nodes are infinite because
+                                            #we don't reach them yet
+            Past_dic[node] = None
 
     #Pour l'arrrivée
-    Load_list[arrival] = [inf, False]
-    Past_list[arrival] = None
-
-    print(Load_list)
-    print(Past_list)
+    Load_dic[arrival] = [inf, False]
+    Past_dic[arrival] = None
 
 
-    #Algorithme de path-finding en lui même
-    #on doit faire tourner l'algo tant que le noeud ayant le point le plus faible est pas arrival (bob)
-    #il faut continuer à faire tourner sinon ca veut dire que le chemin le plus court n'est pas celui allant à arrival
+    #The Dijkstra algorithm is a iterative algorithm: after k iterations, we know
+    #the least cost path to k destinations. Which mean that we are able to know the
+    #least cost path to Bob after the total number of nodes
 
     keep_going = True
 
     for k in range(0,len(nodes)):
+        #We crate a  dictionnary where each key is a relay to each of them only the
+        #load is attribuate
+        temp_load_dic = {}
+        for key, val in Load_dic.items():
+            temp_load_dic[key] = val[0]
 
-        temp_load_list = {}
-        for key, val in Load_list.items():
-            temp_load_list[key] = val[0] #recupere le load
-        
-        node_minimum_load = min(temp_load_list,key=temp_load_list.get)
-        print(Load_list[network['b'][0]][0])
+        node_minimum_load = min(temp_load_dic,key=temp_load_dic.get)
 
+        #We are only interested when the minimum load is the arrival
         if not(node_minimum_load is arrival):
-            for node in nodes:  #je regarde tt les node
-                if Load_list[node][1] == True: #je prends tt ceux ou je suis
-
+            #We are looking at every possible nodes
+            for node in nodes:
+                #But only the nodes where a path already goes to (True for the explored argument)
+                if Load_dic[node][1] == True:
+                    #We take from the network a dictionnary where the key = a node and for each
+                    #key there is all it's neighbors and the cost of the link in order to go to this neighbor
                     neighbours_costs = node.neighbors_and_costs
-                    #neighbours_visited = node.neighbours_visited
+                    #we are looking in this dictionnary to each neighbor and the cost to go to it
+                    for neighbor, cost_to_neighbor in neighbours_costs.items():
+                        if ((Load_dic[node][0] +  cost_to_neighbor) < Load_dic[neighbor][0]) or (Load_dic[neighbor][0] == inf):
+                            #Here we are in the case if the path trought this node + throught the neighbor is less expencive
+                            #than the actual path to the neighbor: indeed the load of the dictionnary contains the cost of all
+                            #the path to it. Or the neighbor has never been visited = the new path is obviously least expencive
+                            ##
+                            #We then fix the new cost for the path which is going to this neighbor, the load part of the load_dic
+                            #and the previous node
+                            Load_dic[neighbor][0] = Load_dic[node][0] +  cost_to_neighbor
+                            Past_dic[neighbor] = node
 
-                    for neighbor, cost_to_neighbor in neighbours_costs.items():    #je regarde tout ceux a qui il est connecté
-                        
-                        #if neighbours_visited[neighbor] == False: #je regarde tt ceux qui n'ont pas encore été parccouru
-                                                                    #elem[beta][1] donne le cout de la liaison entre le kieme noeud et son voisin
-                        if ((Load_list[node][0] +  cost_to_neighbor) < Load_list[neighbor][0]) or (Load_list[neighbor][0] == inf):
-                            #ici on est dans le cas ou le fait de passer par le noeud ou l'on est puis par un des voisins
-                            #de ce noeud est plus interessant que le chemin deja existant(pour aller à ce voisin puisque le load[voisin][1]
-                            # comprend le cout de l'ensemble du chemin jusqu'à lui) OU BIEN que le voisin n'a jamais
-                            #été exploré et donc cest une voie à explorer
-                            Load_list[neighbor][0] = Load_list[node][0] +  cost_to_neighbor
-                            Past_list[neighbor] = node
-                            #neighbours_visited[neighbor] = True
-                            #ici on donne un nouveau cout au fait d'aller à ce voisin ET ON dit d'ou vient le pas antérieur, d'ou arrive
-                            #le chemin
 
-                        Load_list[neighbor][1] = True
+                        Load_dic[neighbor][1] = True
         else:
             keep_going = False
 
-    #sors de la boucle
-    #on doit remonter l'antécedant de chaque membre en noeud en partant du point d'arrivée
+    #We are now out of the loop, we have to reconstruct this least cost path: we have to
+    #get the previous node of each actual node by begining with the arrival and ending
+    #withe departure
 
-    previous_node = Past_list[arrival]
-    while not(previous_node is departure): 
+    previous_node = Past_dic[arrival]
+    while not(previous_node is departure):
         Final_path.append(previous_node)
-        previous_node = Past_list[previous_node]
+        previous_node = Past_dic[previous_node]
 
-    Final_path.append(departure) #on lui met le dernier pas qui est le départ...
+    #The last step is to add the departure to the list
+    Final_path.append(departure)
 
-    #Maintenant il faut inverser la liste pour que le prmier terme soit le départ et pas l'arrivée
+    #We have now to reverse the list in order to get as a first term the departure and not
+    #the arrival...
     Final_path.reverse()
 
     return Final_path
