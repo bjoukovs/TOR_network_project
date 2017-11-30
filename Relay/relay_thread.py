869dc701f2a0453a2d6f625f053dd2647ae8e49e
bjoukovs <borisjoukov@hotmail.com>
2017-11-30 23:40:52 +0100

    commentaires

--- Relay/relay_thread.py
+++ Relay/relay_thread.py
@@ -7,22 +7,22 @@ class Relay(Thread):
     def __init__(self,IP,PORT):
         self._IP = IP
         self._PORT = PORT
-        self._tcp_socket = None
+        self._server_socket = None
         self._thread_running = False
         self._active_clients = []
 
     def create_tcp_socket(self):
         print("Initializing TCP socket",self.IP,self.PORT,end="...")
-        self._tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._tcp_socket.bind((self.IP, self.PORT))
-        self._tcp_socket.setblocking(0)
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.bind((self.IP, self.PORT))
+        self._server_socket.setblocking(0)
         print("OK")
 
     def activate_tcp_socket(self):
         
-        self._tcp_socket.listen(1)
+        self._server_socket.listen(1)
         print("Start listening...",end='')
-        if self._tcp_socket == None:
+        if self._server_socket == None:
             print("ERROR : SOCKET NOT INITIALIZED")
         else:
             Thread.__init__(self)
@@ -48,9 +48,9 @@ class Relay(Thread):
     def close_tcp_socket(self):
         print("Closing TCP socket...",end='')
         if self.isAlive()==True:
-            print("ERROR : THREAD STILL LISTENING, DESACTIVATE_TCP_SOCKET() FIRST")
+            print("ERROR : THREAD STILL LISTENING, DESACTIVATE_server_socket() FIRST")
         else:
-            self._tcp_socket.close()
+            self._server_socket.close()
             print("OK")
 
 
@@ -72,7 +72,7 @@ class Relay(Thread):
 
     @property
     def tcp_socket(self):
-        return self._tcp_socket
+        return self._server_socket
     #UDP IPv4 Socket
 
 
@@ -83,20 +83,25 @@ class Relay(Thread):
             #Des que le socket a du data disponible, readable contient alors la liste des sockets ayant du data.
             #Cependant, il y a un timeout de 1 seconde, ce qui permet que si aucune donnée n'est arrivée dans la seconde, la boucle recommence.
             #Sans cette manipulation, il serait impossible de fermer le thread (_thread_running = false) parce que la commande accept() est bloquante
-            readable,_,_ = select.select(self._active_clients + [self._tcp_socket] , [], [], 1)
+            readable,_,_ = select.select(self._active_clients + [self._server_socket] , [], [], 1)
 
-            for s in readable:
+            for sock in readable:
                 
-                if s is self._tcp_socket:
-                    client_socket, address = self._tcp_socket.accept()
+                #Si le socket qui a reçu un message est _server_socket alors il s'agit d'une demande de connexion
+                if sock is self._server_socket:
+                    client_socket, address = self._server_socket.accept()
                     self._active_clients.append(client_socket)
                     print("Connection from", address)
 
+                #Sinon il s'agit du socket d'un client connecté qui désire envoyer un message
                 else:
-                    data = s.recv(1024)
+                    data = sock.recv(1024)
                     print(data)
                     if data:
-                        s.send(data)
+                        sock.send(data)
+
+                    #Si data est vide cela veux dire fin de connexion
                     else:
-                        s.close()
-                        self._active_clients.remove(s)
+                        print("client déconnecté")
+                        sock.close()
+                        self._active_clients.remove(sock)
