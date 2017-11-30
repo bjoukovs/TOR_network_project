e841efc1b51d61d51f40cae0427ecf7e2c8af4a1
Laurent-St <laurent-st@hotmail.com>
2017-11-30 18:10:59 +0100

    shallot (NON OPERATIONNEL)

--- shared/Message.py
+++ shared/Message.py
@@ -11,6 +11,10 @@ class KEY_INIT(Message):
         self.g=g
         self.p=p
         self.A=A
+    def concatene(self):
+        corpus=(bin(self.version)[2:]+bin(self.type_)[2:]+'00000000'+bin(self.msg_length)[2:]+bin(self.key_id)[2:]
+                +bin(self.g)[2:]+bin(self.p)[2:]+bin(self.A)[2:])
+        return corpus
 
 class KEY_REPLY(Message):
     def __init__(self,version,type_,msg_length,key_id,B):
@@ -18,19 +22,43 @@ class KEY_REPLY(Message):
         self.msg_length=msg_length
         self.key_id=key_id
         self.B=B
+    def concatene(self):
+        corpus=(bin(self.version)[2:]+bin(self.type_)[2:]+'00000000'+bin(self.msg_length)[2:]+bin(self.key_id)[2:]
+                +bin(self.B)[2:])
+        return corpus
 
 class MESSAGE_RELAY(Message):
-    def __init__(self,version,type_,seq_nb,key_id,nexthop,payload,padding):
+    def __init__(self,version,type_,seq_nb,key_id,nexthop,payload):
+        #nexthop=tuple contenant (IP,port) sur 16 bits chacun
         Message.__init__(self,version,type_)
         self.seq_nb=seq_nb
         self.key_id=key_id
-        self.nexthop=nexthop
+        self.nexthop_ip=nexthop[0]
+        self.nexthop_port=nexthop[1]
         self.payload=payload
-        self.padding=padding
+        #self.padding=padding #le padding va etre fait dans la fct concatene et n'est plus un attribut
+    def part_non_cipher(self):
+        corpus=(bin(self.version)[2:]+bin(self.type_)[2:]+'00000000'+bin(self.seq_nb)[2:]+bin(self.key_id)[2:]
+                +self.nexthop_ip) #on considere que nexthop est un nombre binaire en string deja
+        return corpus
+
+    def part_to_cipher(self):
+        corpus=self.nexthop_port+self.payload
+        residual=len(corpus)%32
+        if residual!=0:
+            for i in range(0,residual):
+                corpus=corpus+'0'
+        return corpus
 
 class ERROR(Message):
-    def __init__(self,version,type_,msg_length,error_code,padding):
+    def __init__(self,version,type_,msg_length,error_code):
         Message.__init__(self,version,type_)
         self.msg_length=msg_length
         self.error_code=error_code
-        self.padding=padding
+        #self.padding=padding  #padding fait dans concatene
+    def concatene(self):
+        corpus=(bin(self.version)[2:]+bin(self.type_)[2:]+'00000000'+bin(self.msg_length)[2:]+bin(self.error_code)[2:])
+        if residual!=0:
+            for i in range(0,residual):
+                corpus=corpus+'0'
+        return corpus
---
+++ shared/shallot.py
@@ -0,0 +1,35 @@
+from security import *
+from Message import *
+def negociate_key(sender,receiver):
+    g=2 #A METTRE SUR 32 BITS!
+    #A= ...
+    key_init=KEY_INIT(1,0,2112,key_id,g,A)
+
+def build_shallot(ls_hops,ls_keys,message,sync=0):
+#ATTENTION ls_hops and ls_keys have to be in direct order because we invert them 1 time
+#ls_keys est un tuple (key_id,key)
+#ls_hops= list of tuples (IP,port)
+#ls_hops contains the direct path, but to build the shallot we need to reverse it
+    if sync==0:
+        ls_hops.reverse()
+        ls_keys.reverse()
+    if len(ls_hops)>0: #EN CONSIDERANT QUE ALICE N'EST PAS DANS ls_hops
+        seq_nb=1 #A MODIFIER, QU EST CE QUE CA REPRESENTE
+        key_id=ls_keys[0][0]
+        next_hop=ls_hops[0]
+        message_relay=MESSAGE_RELAY(1,2,seq_nb,key_id,next_hop,message)
+        part=message_relay.part_to_cipher()
+        ciphered=encrypt(ls_keys[0][1],part)
+        ciphertext = ciphertext.decode('unicode_escape').encode('utf-8')
+        print('ciphered:',ciphered.decode('utf-8'))
+        total_message=message_relay.part_non_cipher()+ciphered.decode('utf-8')
+
+        ls_hops.remove(ls_hops[0])
+        ls_keys.remove(ls_keys[0])
+        sync=1;
+        build_shallot(ls_hops,ls_keys,total_message,sync)
+        print('iteration')
+        print('Total message:',total_message)
+
+    if len(ls_hops)==0: #quand ca arrive chez Alice
+        return message
