36a0cfd5256aad0bb2de5b081394658bac8492e8
BenjaminWauthion <benjaminwauthion@gmail.com>
2017-12-18 18:41:32 +0100

    Changements mineurs

--- shared/Path_finding_V3.py
+++ shared/Path_finding_V3.py
@@ -3,11 +3,11 @@ from math import inf
 def Dijkstra(departure, arrival, network):
 
     #We are creating 2 dictionnary
-    Load_dic = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
+    Load_dict = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
                     #there is a tuple. 1st element: the load of the node: the total
                     #cost in order to get to this node. 2nd element: True/False in
                     #in order to know if the node has already been explored
-    Past_dic = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
+    Past_dict = {} #Dictionnary where each key is a relay, or Alice or Bob and to each key
                     #it is assign the previous node, from where come from the message
                     #before arriving at this node
 
@@ -21,20 +21,20 @@ def Dijkstra(departure, arrival, network):
 
     #initization
     #For the departure
-    Load_dic[departure] = [0, True] #The cost is null because we begin from this node and we come
+    Load_dict[departure] = [0, True] #The cost is null because we begin from this node and we come
                                     #from this node: (True)
-    Past_dic[departure] = None #This is the departure, it has no past nodes
+    Past_dict[departure] = None #This is the departure, it has no past nodes
 
     #For all intermediate relays
     for i, node in enumerate(nodes):
         if not(node is arrival) and not(node is departure):
-            Load_dic[node] = [inf, False] #The cost for these nodes are infinite because
+            Load_dict[node] = [inf, False] #The cost for these nodes are infinite because
                                             #we don't reach them yet
-            Past_dic[node] = None
+            Past_dict[node] = None
 
     #Pour l'arrrivée
-    Load_dic[arrival] = [inf, False]
-    Past_dic[arrival] = None
+    Load_dict[arrival] = [inf, False]
+    Past_dict[arrival] = None
 
 
     #The Dijkstra algorithm is a iterative algorithm: after k iterations, we know
@@ -46,35 +46,35 @@ def Dijkstra(departure, arrival, network):
     for k in range(0,len(nodes)):
         #We crate a  dictionnary where each key is a relay to each of them only the
         #load is attribuate
-        temp_load_dic = {}
-        for key, val in Load_dic.items():
-            temp_load_dic[key] = val[0]
+        temp_load_dict = {}
+        for key, val in Load_dict.items():
+            temp_load_dict[key] = val[0]
 
-        node_minimum_load = min(temp_load_dic,key=temp_load_dic.get)
+        node_minimum_load = min(temp_load_dict,key=temp_load_dict.get)
 
         #We are only interested when the minimum load is the arrival
         if not(node_minimum_load is arrival):
             #We are looking at every possible nodes
             for node in nodes:
                 #But only the nodes where a path already goes to (True for the explored argument)
-                if Load_dic[node][1] == True:
+                if Load_dict[node][1] == True:
                     #We take from the network a dictionnary where the key = a node and for each
                     #key there is all it's neighbors and the cost of the link in order to go to this neighbor
                     neighbours_costs = node.neighbors_and_costs
                     #we are looking in this dictionnary to each neighbor and the cost to go to it
                     for neighbor, cost_to_neighbor in neighbours_costs.items():
-                        if ((Load_dic[node][0] +  cost_to_neighbor) < Load_dic[neighbor][0]) or (Load_dic[neighbor][0] == inf):
+                        if ((Load_dict[node][0] +  cost_to_neighbor) < Load_dict[neighbor][0]) or (Load_dict[neighbor][0] == inf):
                             #Here we are in the case if the path trought this node + throught the neighbor is less expencive
                             #than the actual path to the neighbor: indeed the load of the dictionnary contains the cost of all
                             #the path to it. Or the neighbor has never been visited = the new path is obviously least expencive
                             ##
                             #We then fix the new cost for the path which is going to this neighbor, the load part of the load_dic
                             #and the previous node
-                            Load_dic[neighbor][0] = Load_dic[node][0] +  cost_to_neighbor
-                            Past_dic[neighbor] = node
+                            Load_dict[neighbor][0] = Load_dict[node][0] +  cost_to_neighbor
+                            Past_dict[neighbor] = node
 
 
-                        Load_dic[neighbor][1] = True
+                        Load_dict[neighbor][1] = True
         else:
             keep_going = False
 
@@ -82,10 +82,10 @@ def Dijkstra(departure, arrival, network):
     #get the previous node of each actual node by begining with the arrival and ending
     #withe departure
 
-    previous_node = Past_dic[arrival]
+    previous_node = Past_dict[arrival]
     while not(previous_node is departure):
         Final_path.append(previous_node)
-        previous_node = Past_dic[previous_node]
+        previous_node = Past_dict[previous_node]
 
     #The last step is to add the departure to the list
     Final_path.append(departure)
